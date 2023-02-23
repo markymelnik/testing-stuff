c86704f170753fa1195adbf1b5882305b3ecb94d
markymelnik <markemelnik@gmail.com>
2023-02-23 16:41:34 -0500

    Additional tests

--- testing.test.js
+++ testing.test.js
@@ -20,4 +20,38 @@ test('adding positive numbers is not zero', () => {
   }
 });
 
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+})
+
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+})
+
+test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2;
+  expect(value).not.toBe(0.3);
+  expect(value).toBeCloseTo(0.3);
+})
+
 // 'npm test'
